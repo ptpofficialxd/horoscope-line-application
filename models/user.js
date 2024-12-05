27afaffb55c2952c78220992b1537a490313d4f0
ptpofficialxd <aekkungzaza@gmail.com>
2024-12-06 03:44:19 +0700

    update userschema

--- models/user.js
+++ models/user.js
@@ -2,15 +2,21 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   lineId: String,
+  userType: String,
   firstName: String,
   lastName: String,
   phone: String,
+  gender: String,
   birthdate: Date,
   age: Number,
-  gender: String,
-  userType: String,
   selfDescription: String,
   branch: String,
+  serviceHours: {
+    start: String,
+    end: String,
+  },
+  profilePicture: String,
+  certificate: String,
   createdAt: Date,
 });
 
