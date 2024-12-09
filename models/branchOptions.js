04597d5909f6d7a4c12ed000431a2e37a3fb0982
ptpofficialxd <aekkungzaza@gmail.com>
2024-12-09 13:50:31 +0700

    create branchOptions for astrologer

---
+++ models/branchOptions.js
@@ -0,0 +1,8 @@
+export const branchOptions = [
+    { value: "astrology", label: "โหราศาสตร์" },
+    { value: "numerology", label: "เลขศาสตร์" },
+    { value: "tarot", label: "ทำนายไพ่" },
+    { value: "palmistry", label: "ลายมือ" },
+    { value: "zodiac", label: "ราศี" },
+    { value: "fengshui", label: "ฮวงจุ้ย" },
+  ];
